441cfd1a2bd87d5c201a8560df45fc39ca58558f
liuzhe <liu748951@163.com>
2020-12-08 09:02:47 +0800

    Update Calculate.js

--- src/Calculate.js
+++ src/Calculate.js
@@ -14,6 +14,83 @@ import { SearchOutlined, DeleteOutlined, QuestionCircleOutlined, InfoCircleOutli
 import locale from 'antd/es/date-picker/locale/zh_CN';
 import copy from 'copy-to-clipboard';
 
+
+const columns2 = [
+    {
+        title: 'Full Name',
+        width: 100,
+        dataIndex: 'name',
+        key: 'name',
+        fixed: 'left',
+    },
+    {
+        title: 'Age',
+        width: 100,
+        dataIndex: 'age',
+        key: 'age',
+        fixed: 'left',
+    },
+    {
+        title: 'Column 1',
+        dataIndex: 'address',
+        key: '1',
+        width: 150,
+    },
+    {
+        title: 'Column 2',
+        dataIndex: 'address',
+        key: '2',
+        width: 150,
+    },
+    {
+        title: 'Column 3',
+        dataIndex: 'address',
+        key: '3',
+        width: 150,
+    },
+    {
+        title: 'Column 4',
+        dataIndex: 'address',
+        key: '4',
+        width: 150,
+    },
+    {
+        title: 'Column 5',
+        dataIndex: 'address',
+        key: '5',
+        width: 150,
+    },
+    {
+        title: 'Column 6',
+        dataIndex: 'address',
+        key: '6',
+        width: 150,
+    },
+    {
+        title: 'Column 7',
+        dataIndex: 'address',
+        key: '7',
+        width: 150,
+    },
+    { title: 'Column 8', dataIndex: 'address', key: '8' },
+    {
+        title: 'Action',
+        key: 'operation',
+        width: 100,
+        render: () => <a>action</a>,
+    },
+];
+
+const data2 = [];
+for (let i = 0; i < 8; i++) {
+    data2.push({
+        key: i,
+        name: `Edrward ${i}`,
+        age: 32,
+        address: `London Park no. ${i}`,
+    });
+}
+
 //模型计算接口地址
 const url = "http://139.217.82.132:5000/";
 /**
@@ -815,6 +892,8 @@ export default class Calculate extends Component {
                         <Button className="custom-btn calculate-btn" onClick={this.handleSubmit} type="primary">计算</Button>
                     </div>
                     <Modal title="模型计算结果" visible={modalVisible} onOk={this.openResModal} onCancel={this.closeResModal} footer={null}>
+                    {/* <Modal title="模型计算结果" visible={true} onOk={this.openResModal} onCancel={this.closeResModal} footer={null}> */}
+                        <Table columns={columns2} dataSource={data2} scroll={{ x: 1500 }} pagination={false} />
                         <Result
                             status={calcStatus ? "success" : "error"}
                             title={calcStatus ? modelName + "模型计算成功!" : modelName + "模型计算失败!"}
